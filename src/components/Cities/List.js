15ffd76b02f3b2c383efa452629c1717aa199e53
Victor Silva <vsilva.programador@gmail.com>
2018-09-22 19:13:13 -0300

    add title and loading placeholder

--- src/components/Cities/List.js
+++ src/components/Cities/List.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { css } from 'react-emotion';
-
+// Layout components
 import Ul from '../Layout/Ul';
+import SectionTitle from '../Layout/SectionTitle';
+import LoaderPage from '../Layout/LoaderPage';
+// City List Item
 import City from './City';
+// Assets
+import loading from '../../assets/loading.svg';
 
 /**
  * List of Cities.
@@ -12,12 +17,14 @@ class List extends Component {
     super(props);
 
     this.state = {
-      cities: []
+      cities: [],
+      isLoading: true
     };
   }
 
   async fetchCitiesOf(state) {
-    if (process.env.NODE_ENV !== 'development') {
+			this.props.onLoadItems();
+			this.setState({ isLoading: true });
       let response, cities = [];
 
       try {
@@ -31,13 +38,9 @@ class List extends Component {
       }
       
       this.setState({
-        cities
+        cities,
+        isLoading: false
       });
-    } else {
-      this.setState({
-        cities: [{"id":1,"name":"Acrelândia","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":2,"name":"Assis Brasil","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":3,"name":"Brasiléia","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":4,"name":"Bujari","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":5,"name":"Capixaba","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":6,"name":"Cruzeiro do Sul","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":7,"name":"Epitaciolândia","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":8,"name":"Feijó","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":9,"name":"Jordão","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":10,"name":"Mâncio Lima","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":11,"name":"Manoel Urbano","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":12,"name":"Marechal Thaumaturgo","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":13,"name":"Plácido de Castro","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":14,"name":"Porto Acre","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":15,"name":"Porto Walter","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":16,"name":"Rio Branco","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":17,"name":"Rodrigues Alves","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":18,"name":"Santa Rosa do Purus","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":19,"name":"Sena Madureira","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":20,"name":"Senador Guiomard","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":21,"name":"Tarauacá","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}},{"id":22,"name":"Xapuri","state_id":1,"created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42","state":{"id":1,"name":"Acre","uf":"AC","created_at":"2018-09-18 21:58:42","updated_at":"2018-09-18 21:58:42"}}]
-      });
-    }
   }
 
   /**
@@ -60,18 +63,25 @@ class List extends Component {
   }
 
   render() {
-    return (
-      <Ul className={css`
-        pointer-events: ${this.props.pointerEvents? 'all': 'none'};
-        overflow-y: ${this.props.pointerEvents? 'auto': 'hidden'}
-      `}>
-        {
-          this.state.cities.map(city => (
-            <City key={city.id} name={city.name}/>
-          ))
-        }
-      </Ul>
-    );
+    let component;
+    if (this.state.isLoading) {
+      component = <LoaderPage icon={loading}/>
+    } else {
+      component = (
+        <Ul className={css`
+          pointer-events: ${this.props.pointerEvents? 'all': 'none'};
+          overflow-y: ${this.props.pointerEvents? 'auto': 'hidden'}
+        `}>
+					<SectionTitle>Cidades</SectionTitle>
+          {
+            this.state.cities.map(city => (
+              <City key={city.id} name={city.name}/>
+            ))
+          }
+        </Ul>
+      );
+    }
+    return component;
   }
 }
 
