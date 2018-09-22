412442096337f4a3af72c276494f7f91324c7cec
Victor Silva <vsilva.programador@gmail.com>
2018-09-22 19:14:19 -0300

    update nav width

--- src/components/Layout/Grid.js
+++ src/components/Layout/Grid.js
@@ -3,6 +3,6 @@ import styled from 'react-emotion';
 export default styled('div')`
   @media (min-width: 768px) {
     display: grid;
-    grid-template-columns: 35% auto;
+    grid-template-columns: minmax(300px, 25%) auto;
   }
 `;
