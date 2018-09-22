c2fa8abc7b3f2b11c01545aac0db56cb22c4dbe2
Victor Silva <vsilva.programador@gmail.com>
2018-09-22 19:12:34 -0300

    add list title component

---
+++ src/components/Layout/SectionTitle.js
@@ -0,0 +1,10 @@
+import styled from 'react-emotion';
+
+import Title from './Title.js';
+
+export default styled(Title)`
+  text-align: center;
+  color: #5d636b;
+  font-size: 1.5em;
+  margin: 2em 0;
+`;
