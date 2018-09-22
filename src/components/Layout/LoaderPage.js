cd427bb450b814e080a391b1403e21ee48f7a54f
Victor Silva <vsilva.programador@gmail.com>
2018-09-22 19:11:11 -0300

    add loader placeholder

---
+++ src/components/Layout/LoaderPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { css } from 'react-emotion';
+
+const loader = icon => css`
+  background: url(${icon});
+  height: calc(100vh - 65px);
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+
+export default function({ icon }) {
+	return <div className={loader(icon)}></div>;
+}
