e3023684470bf040f4b7b7bd03d171394e076f8f
Victor Silva <vsilva.programador@gmail.com>
2018-09-22 13:47:02 -0300

    use hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import styled from 'react-emotion';
 
 import StatesList from './components/States/List';
