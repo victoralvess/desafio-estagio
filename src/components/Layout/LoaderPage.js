bd73fa778bd88ce9f299b7af13bc014feab9da65
Victor Silva <vsilva.programador@gmail.com>
2018-09-23 09:28:57 -0300

    refactor loader

--- src/components/Layout/LoaderPage.js
+++ src/components/Layout/LoaderPage.js
@@ -1,16 +1,11 @@
-import React from 'react';
-import { css } from 'react-emotion';
+import styled from 'react-emotion';
 
 import { toolbar } from '../../styles/dimensions';
 
-const loader = icon => css`
-  background: url(${icon});
+export default styled('div')`
+  background: url(${({ icon }) => icon});
   height: calc(100vh - ${toolbar.height});
   background-size: contain;
   background-repeat: no-repeat;
   background-position: center;
 `;
-
-export default function({ icon }) {
-	return <div className={loader(icon)}></div>;
-}
